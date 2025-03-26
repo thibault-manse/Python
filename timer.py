808a9c99d19e51fd182acff63f629e899c7662da
christine chemali <christine.chemali@laplateforme.io>
2025-03-26 12:29:09 +0100

    update start method

--- timer.py
+++ timer.py
@@ -4,9 +4,14 @@ class Timer:
     def __init__(self):
         self.elapsed_time = 0
         self.is_running = False
+        self.is_paused = True
+        self.start_time = 0
     
     def start(self):
         """ To start the timer """
-        self.is_running = True
-        self.start_time = time.time()
+        if not self.is_running:
+            self.is_running = True
+            self.is_paused = False
+            self.start_time = time.time()
+
     

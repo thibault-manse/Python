b162d2527fa87557af9ec26410b3b032db680d22
christine chemali <christine.chemali@laplateforme.io>
2025-03-26 12:50:34 +0100

    rename methods for more clarity

--- timer.py
+++ timer.py
@@ -7,14 +7,14 @@ class Timer:
         self.is_paused = True
         self.start_time = 0
     
-    def start(self):
+    def start_timer(self):
         """ To start the timer """
         if not self.is_running:
             self.is_running = True
             self.is_paused = False
             self.start_time = time.time()
 
-    def pause(self):
+    def pause_timer(self):
         """ Pause or restart the timer """
         if self.is_running:
             self.is_running = False
@@ -23,19 +23,19 @@ class Timer:
         elif self.is_paused:
             self.start()
     
-    def stop(self):
+    def stop_timer(self):
         """ Stop the timer """
         if self.is_running:
             self.is_running = False
             self.elapsed_time += time.time() - self.start_time 
 
-    def reset(self):
+    def reset_timer(self):
         """ Reset the timer """
         self.elapsed_time = 0
         self.is_running = False
         self.is_paused = True
 
-    def update(self):
+    def update_timer(self):
         """ update the elapsed time """
         if self.is_running:
             return self.elapsed_time + (time.time())

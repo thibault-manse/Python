8fe1437ab150912d0ad758de8844444af9ea3936
christine chemali <christine.chemali@laplateforme.io>
2025-03-27 20:38:22 +0100

    add create label info label method to GameInfo class

--- game_info.py
+++ game_info.py
@@ -10,3 +10,13 @@ class GameInfo:
         
         self.create_info_labels()
         
+    def create_info_labels(self):
+        """ To display info label """
+        self.mines_label = ctk.CTkLabel(self.root, text=f"Mines restantes : {self.mines}", font=("Arial", 14))
+        self.mines_label.pack(pady=5)
+        
+        self.flags_label = ctk.CTkLabel(self.root, text=f"Drapeaux : {self.flags}", font=("Arial", 14))
+        self.flags_label.pack(pady=5)
+        
+        self.question_marks_label = ctk.CTkLabel(self.root, text=f"Points d'interrogation : {self.question_marks}", font=("Arial", 14))
+        self.question_marks_label.pack(pady=5)

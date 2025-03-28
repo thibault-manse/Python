0b65adc9c6bbf6a97f49273c1fa9deab8ce1af3a
christine chemali <christine.chemali@laplateforme.io>
2025-03-28 15:14:07 +0100

    add method to update the score in the database

--- user_operation.py
+++ user_operation.py
@@ -14,10 +14,19 @@ class UserOperations:
         try:
             self.cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
             self.connection.commit()
-            messagebox.showinfo("Info", f"'{username}', vous êtes enregistré.")
+            messagebox.showinfo("Info", f"Bienvenue {username}.")
             return True
         except mysql.connector.IntegrityError:
-            messagebox.showinfo("Info",f"'{username}', vous êtes déja enregistré.")
+            messagebox.showinfo("Info",f"{username}, vous avez déja un compte.")
             return False
         except Error as error:
             messagebox.showerror(f"Une erreur est survenue lors de l'enregistrement : {error}")
+
+    def update_score(self, user_id, score):
+        """To update the score for a registred user """
+        try:
+            self.cursor.execute("INSERT INTO scores (user_id, score) VALUES (%s, %s)", (user_id, score))
+            self.connection.commit()
+            messagebox.showinfo("Info","Votre score a été mis à jour.")
+        except Error as error:
+            messagebox.showerror("Erreur", f"Votre score n'a pas pu être mis à jour : {error}")
